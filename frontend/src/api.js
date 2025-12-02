7db41ea951f1ed9040bd2bed5ca737a5ea65550c
Veerendra Kumar K R <veerendra2137@gmail.com>
2025-12-02 20:50:53 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://ats-linkedin.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
