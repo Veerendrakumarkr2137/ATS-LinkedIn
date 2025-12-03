0ddfac3de8b3137014d8007d68a83f6f425af3ce
Veerendrakumarkr2137 <veerendra2137@gmail.com>
2025-12-03 20:44:29 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,20 @@
-// src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
-import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ResumeAnalyzer from "./pages/ResumeAnalyzer";
+import Dashboard from "./pages/Dashboard";
+import ResumeATS from "./pages/ResumeAnalyzer.jsx";
 import LinkedInAnalyzer from "./pages/LinkedInAnalyzer";
-import "./App.css";
-import "./index.css";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="app">
-          <Navbar />
-          <main className="main">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/resume"
-                element={
-                  <ProtectedRoute>
-                    <ResumeAnalyzer />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/linkedin"
-                element={
-                  <ProtectedRoute>
-                    <LinkedInAnalyzer />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/resume" element={<ResumeATS />} />
+        <Route path="/linkedin" element={<LinkedInAnalyzer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
